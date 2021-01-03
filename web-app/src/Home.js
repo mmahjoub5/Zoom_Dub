4fcf0fa13842095e697148e2ceef8b90f809c536
Amin Mahjoub <mahjoub@usc.edu>
2021-01-03 03:29:38 -0800

    changes

--- web-app/src/Home.js
+++ web-app/src/Home.js
@@ -20,7 +20,7 @@ class Home extends Component {
     }
 
     render() { 
-      if(this.state.redirect) return ( <RTCMesh URL="ws://localhost:8080/socket"/> )
+      if(this.state.redirect) return ( <RTCMesh URL="ws://192.168.1.228:8080/socket"/> )
       return ( 
         <MuiThemeProvider>
         
