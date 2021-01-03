4304cf33c1c335095fbb231d455a4fa6bf7a10ba
Robb Tran <bobtran@usc.edu>
2021-01-02 21:00:22 -0800

    push

---
+++ dubzoom/src/main/java/com/dubzoom/DubzoomApplication.java
@@ -0,0 +1,12 @@
+package com.dubzoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubzoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubzoomApplication.class, args);
+	}
+}
--- dubzoom/src/main/resources/data-mysql.sql
+++ dubzoom/src/main/resources/data-mysql.sql
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 insert into users (id, fname, lname, email, password) values (2, "jo", "everyday", "ithurts@gmail.com", "somuch")
+=======
+insert into users (id, fname, lname, email, password) values (1, "jo", "andgo", "jo@gmail.com", "andgo")
+>>>>>>> Stashed changes
